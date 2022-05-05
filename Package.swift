fa3e1d7535a7aaeb6e8ec9bf72de2166e6b6782b
Арман Чархчян <re-mont1@mail.ru>
2022-05-05 19:48:44 +0400

    fix

--- Package.swift
+++ Package.swift
@@ -3,16 +3,10 @@
 
 import PackageDescription
 
-private let remoteDependencies: [Package.Dependency] = [
+private let dsependencies: [Package.Dependency] = [
     .package(url: "https://github.com/arman095095/Module.git", branch: "develop"),
 ]
 
-private let localDependencies: [Package.Dependency] = [
-    .package(path: "/Users/armancarhcan/Desktop/Workdir/Module"),
-]
-
-let isDev = true
-private let dependencie = isDev ? localDependencies : remoteDependencies
 
 let package = Package(
     name: "SettingsRouteMap",
@@ -22,7 +16,7 @@ let package = Package(
             name: "SettingsRouteMap",
             targets: ["SettingsRouteMap"]),
     ],
-    dependencies: dependencie,
+    dependencies: dependencies,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
